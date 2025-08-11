ecd3f11493f38c6a4c22342c5d99aaa2debe3d1f
sayemx <sayem.saqueeb@splashbi.com>
2025-08-11 17:58:07 +0530

    Section 6: Implemented the Spring cloud config server using classpath and then git and used spring cloud bus (rabbitmq) to refresh.

--- src/main/java/com/sayem/loans/dto/LoansContactInfoDto.java
+++ src/main/java/com/sayem/loans/dto/LoansContactInfoDto.java
@@ -2,9 +2,18 @@ package com.sayem.loans.dto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 import java.util.Map;
 
+@Getter @Setter
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class LoansContactInfoDto {
+	
+	private String message;
+	private Map<String, String> contactDetails; 
+	private List<String> onCallSupport;
+
 }
